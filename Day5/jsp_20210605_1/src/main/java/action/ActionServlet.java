5b627d30af1829423d6889e8b836cac57868d7be
xlxllxlx88 <xlxllxlx88@gmail.com>
2021-06-05 11:06:02 +0900

    Day5

---
+++ Day5/jsp_20210605_1/src/main/java/action/ActionServlet.java
@@ -0,0 +1,61 @@
+package action;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ActionServlet
+ */
+@WebServlet("/action.do")
+public class ActionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ActionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 인자 -> request -> DTO/VO -> DB저장(DAO)
+		// 3개의 전화번호 필트 -> 1개의 전화번호 DB table 필드
+		Map<String,String[]> map = request.getParameterMap();
+		
+		
+		// 인자가 넘어오면 로그를 먼저 찍어보세요
+		// 인자확인 -람다?
+		map.forEach((k, v) -> System.out.println(k+"="+v[0]));
+		map.keySet().forEach(x -> System.out.println(x + "=" + map.get(x)[0]));
+		
+		// map.entrySet().forEach(x->System.out.println(x));
+		// map.entrySet().forEach(System.out::println);
+		
+		// DAO
+		// ex) dao.insertMember(memberVO);
+		
+		
+		} 
+		
+		
+
+}
