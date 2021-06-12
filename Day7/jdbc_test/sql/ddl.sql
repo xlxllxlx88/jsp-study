4124720a4751c807ecbe9d9b64535593ef28dd71
xlxllxlx88 <xlxllxlx88@gmail.com>
2021-06-12 16:20:20 +0900

    Day7

---
+++ Day7/jdbc_test/sql/ddl.sql
@@ -0,0 +1,35 @@
+CREATE TABLE member_tbl(
+	id varchar(20) PRIMARY KEY,
+	pw varchar(20) NOT NULL,
+	name varchar(50) NOT NULL,
+	address varchar(500),
+	joindate date DEFAULT now()
+);
+
+CREATE TABLE demo2(
+	joidate date DEFAULT now()
+);
+
+INSERT INTO demo2 values (now());
+
+SELECT * FROM demo2;
+
+DROP TABLE IF EXISTS member_tbl;
+
+--------------------------------------------------------------
+
+SELECT * FROM member_tbl;
+
+INSERT INTO
+	member_tbl
+VALUES (
+	 'java', '0000', '김철수', '서울 강서구 화곡동', now()
+);
+
+UPDATE member_tbl
+   SET pw = '1111'
+	 , address = '서울 강남구 역삼동'
+WHERE id='java';
+
+DELETE FROM MEMBER_TBl WHERE id='java';
+
--- Day7/jdbc_test/src/main/java/com/javateam/project/util/DbUtil.java
+++ Day7/jdbc_test/src/main/java/com/javateam/project/util/DbUtil.java
@@ -1,4 +1,4 @@
-package util;
+package com.javateam.project.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
