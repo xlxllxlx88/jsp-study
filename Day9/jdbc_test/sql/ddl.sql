03d33c7d541c548bc21b501b0f373c3b78d6144a
xlxllxlx88 <xlxllxlx88@gmail.com>
2021-06-20 14:08:03 +0900

    Day9

--- Day9/jdbc_test/sql/ddl.sql
+++
@@ -1,9 +0,0 @@
--- 회원 테이블
-CREATE TABLE member_tbl (
-	id varchar(20) PRIMARY KEY,
-	pw varchar(20) NOT NULL,
-	name varchar(50) NOT NULL,
-	address varchar(500),
-	joindate date DEFAULT now()
-);
-
